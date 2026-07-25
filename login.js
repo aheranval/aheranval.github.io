0b92d450e70406e7571f516c512a6f03d4ef2d4a
Alice Heranval <106564849+aheranval@users.noreply.github.com>
2026-07-24 20:19:18 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,30 @@
-
+document
+.getElementById("login-button")
+.addEventListener(
+"click",
+async () => {
+    const email =
+        document
+        .getElementById("email")
+        .value;
+    const password =
+        document
+        .getElementById("password")
+        .value;
+    const {
+        error
+    } = await supabaseClient.auth.signInWithPassword({
+        email,
+        password
+    });
+    if(error){
+        alert(error.message);
+        return;
+    }
+    document
+        .getElementById("login-section")
+        .style.display = "none";
+    document
+        .getElementById("admin-section")
+        .style.display = "block";
+});
