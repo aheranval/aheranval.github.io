3ccb98c1c345f7481c1be18c395450e33b5fac9f
Alice Heranval <106564849+aheranval@users.noreply.github.com>
2026-07-24 20:25:05 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,34 +1,73 @@
-const {
-    data
-} = await supabaseClient.auth.getSession();
+async function checkLogin() {
+
+    const {
+        data
+    } = await supabaseClient.auth.getSession();
+
+
+    if (data.session) {
+
+        document
+            .getElementById("login-section")
+            .style.display = "none";
+
+        document
+            .getElementById("admin-section")
+            .style.display = "block";
+
+    }
+
+}
+
+
+checkLogin();
+
+
 
 document
 .getElementById("login-button")
 .addEventListener(
 "click",
 async () => {
+
     const email =
         document
         .getElementById("email")
         .value;
+
+
     const password =
         document
         .getElementById("password")
         .value;
+
+
     const {
         error
     } = await supabaseClient.auth.signInWithPassword({
+
         email,
         password
+
     });
+
+
     if(error){
+
         alert(error.message);
+
         return;
+
     }
+
+
     document
         .getElementById("login-section")
         .style.display = "none";
+
+
     document
         .getElementById("admin-section")
         .style.display = "block";
+
 });
